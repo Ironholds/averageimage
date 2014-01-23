524e2cb29651855aebd53351cfa47e04c2574c11
Oliver Keyes <ironholds@gmail.com>
2014-01-22 17:28:12 -0800

    start up the actual image composition classes

---
+++ R/ComposerClasses.R
@@ -0,0 +1,114 @@
+PNGClass <- setRefClass(Class = "PNGClass",
+                        fields = list(
+                          imagenames = "character",
+                          retrieval_type = "character",
+                          samplesize = "numeric",
+                          images = "list"),
+                        methods = list(
+                          
+                          #Method for reading the image in
+                          reader = function(){
+                            
+                            
+                            
+                          },
+                          
+                          #Method for trimming down odd-sized images
+                          trimmer = function(){
+                            
+                            
+                          },
+                          
+                          #Method for actually, well, generating the composites ;p
+                          compositor = function(){
+                            
+                            #Image to export
+                            composite_image = NULL
+                            
+                            #For each image...
+                            for(i in seq_along(.self$images)){
+                              
+                              if(is.null(composite_image)){
+                                
+                                #If it's the first image to be read, just shadow composite_image with it
+                                composite_image <- .self$images[[i]]
+                              
+                              } else {
+                                
+                                #Otherwise, add the values
+                                composite_image <- composite_image + .self$images[[i]]
+                              }
+                              
+                            }
+                            
+                            #Average
+                            composite_image <- composite_image/length(.self$images)
+                            
+                            #Return
+                            return(composite_image)
+                          },
+                          
+                          #General method
+                          generator = function(){
+                            
+                            #Read files in
+                            .self$reader()
+                            
+                            #Trim them down
+                            .self$trimmer()
+                            
+                            #Generate a composite
+                            composite_image <- .self$compositor()
+                            
+                            #Save it
+                            writePNG(image = composite_image,
+                                     target = save.file)
+                          }
+                        
+                        )
+)
+
+JPEGClass <- setRefClass(Class = "JPEGClass",
+                         contains = "PNGClass",
+                         methods = list(
+                           
+                          #Method for reading the image in - shadows PNGClass$reader()
+                          reader = function(){
+                           
+                           
+                           
+                          },
+                          
+                          #Grouped function - shadows PNGClass$generator()
+                          generator = function(){
+                             
+                            #Read files in
+                            .self$reader()
+                            
+                            #Trim them down
+                            .self$trimmer()
+                            
+                            #Generate a composite
+                            composite_image <- .self$compositor()
+                             
+                            #Save it
+                            writeJPEG(image = composite_image,
+                                      target = save.file,
+                                      quality = 1)
+                           }
+                        )
+)
+
+TIFFClass <- setRefClass(Class = "TIFFClass",
+                         contains = "PNGClass",
+                         methods = list(
+                           
+                         )
+)
+
+BMPClass <- setRefClass(Class = "BMPClass",
+                        contains = "PNGClass",
+                        methods = list(
+                          
+                        )
+)
