6b099b7558401b4f533a639ae0732ee7afd6c20f
Oliver Keyes <ironholds@gmail.com>
2014-01-23 11:30:08 -0800

    bugfixes and license

--- R/ComposerClasses.R
+++ R/ComposerClasses.R
@@ -1,7 +1,27 @@
+# Copyright (c) 2013 Oliver Keyes
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 PNGClass <- setRefClass(Class = "PNGClass",
                         fields = list(
                           imagenames = "character",
                           retrieval_type = "character",
+                          savefile = "character",
                           images = "list"),
                         methods = list(
                           
@@ -138,7 +158,7 @@ PNGClass <- setRefClass(Class = "PNGClass",
                             
                             #Save it
                             writePNG(image = composite_image,
-                                     target = save.file)
+                                     target = .self$savefile)
                           }
                         
                         )
@@ -196,7 +216,7 @@ JPEGClass <- setRefClass(Class = "JPEGClass",
                              
                             #Save it
                             writeJPEG(image = composite_image,
-                                      target = save.file,
+                                      target = .self$savefile,
                                       quality = 1)
                           }
                         )
