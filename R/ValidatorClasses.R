3136d8167f128a65db57bd75059adee1b0f3686e
Oliver Keyes <ironholds@gmail.com>
2014-01-23 16:58:25 -0800

    Update depends in DESCRIPTION

--- R/ValidatorClasses.R
+++ R/ValidatorClasses.R
@@ -17,7 +17,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-#Class for file validation
 FileClass <- setRefClass(Class = "FileClass",
                          fields = list(
                            data = "character",
