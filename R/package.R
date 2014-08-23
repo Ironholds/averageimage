7c760330fba48c647640e273304250ae98b6dc7f
Oliver Keyes <ironholds@gmail.com>
2014-08-22 21:30:54 -0400

    update package documentation

--- R/package.R
+++ R/package.R
@@ -1,7 +1,13 @@
+#' @title meanit
+#' 
+#' @description composite image generation in R
+#' 
+#' @details
 #' meanit is an R library designed to generate composite PNGs or JPEGs.
-#' In this regard it marks R's rapid approach to the threshold of Zawinski's law. Other
-#' than the automated generation of beautiful composite images associated with, say,
-#' open data and picture releases, the library is of relatively limited utility.
+#' In this regard it marks R's rapid approach to the threshold of Zawinski's law.
+#' Prototypes have been used in the generation of \href{http://blog.ironholds.org/art-as-data-as-art-part-ii/}{composite images from open
+#' dataset releases}.
+#' 
 #' @docType package
 #' @name meanit-package
 NULL
